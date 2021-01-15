09862e2f8920e163bb7f0485ec2f47d4928dde3a
Angela <angela.koepp@gmail.com>
2021-01-16 00:17:52 +1300

    Initial project structure

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using BookApi.Models;
+using BookApi.OpenApi;
+using BookApi.Store;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Consumes(contentType: ContentTypes.APPLICATION_JSON)]
+    [Produces(contentType: ContentTypes.APPLICATION_JSON)]
+    public class BooksController : ControllerBase
+    {
+        private IBookStore _bookStore;
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(IBookStore bookStore, ILogger<BooksController> logger)
+        {
+            _bookStore = bookStore;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get the list of all books
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<Book>> Get()
+        {
+            return await _bookStore.GetBooks();
+        }
+    }
+}
---
+++ BookApi/Controllers/ServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        public ActionResult Ping()
+        {
+            return Ok("Service is up");
+        }
+    }
+}
